0d09be7cb973e225c68d4742eb84ca98aca4fa25
Aaqeb11 <aaqebahmed@gmail.com>
2026-03-18 20:14:46 +0300

    chore: add host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  allowedHosts: "all",
   server: {
     port: 5173,
     proxy: {
